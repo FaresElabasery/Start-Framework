8213445f9b6fc0a083f198cc04a987fedb874ac0
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-08-01 05:50:12 +0300

    fix: fix font weight in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import Container from '../Container/Container'
-import { TfiWorld } from 'react-icons/tfi'
 import { FaFacebook, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+import { TfiWorld } from 'react-icons/tfi'
+import Container from '../Container/Container'
 
 export default function Footer() {
   return (
@@ -9,12 +8,12 @@ export default function Footer() {
       <Container>
         <div className='row'>
           <div className='w-full sm:w-1/3 mb-16 text-center'>
-            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-normal mb-1'>Location</h2>
+            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-semibold mb-1'>Location</h2>
             <p>2215 John Daniel Drive</p>
             <p className='mt-4'>Clark, MO 65243</p>
           </div>
           <div className='w-full sm:w-1/3 mb-20 text-center'>
-            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-normal mb-1'>AROUND THE WEB</h2>
+            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-semibold mb-1'>AROUND THE WEB</h2>
             <ul className='row gap-2 justify-center'>
               <li className='social-icons'><a href="https://www.facebook.com/fares.elabasery.9"></a><FaFacebook className='size-5' /></li>
               <li className='social-icons'><a href="https://github.com/FaresElabasery?tab=repositories"></a><FaTwitter className='size-5' /></li>
@@ -23,7 +22,7 @@ export default function Footer() {
             </ul>
           </div>
           <div className='w-full sm:w-1/3 mb-20 text-center'>
-            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-normal mb-1'>ABOUT FREELANCER</h2>
+            <h2 className='uppercase text-[clamp(24px,5vw,28px)] font-semibold mb-1'>ABOUT FREELANCER</h2>
             <p>Freelance is a free to use, licensed Bootstrap theme created by Route</p>
           </div>
         </div>
