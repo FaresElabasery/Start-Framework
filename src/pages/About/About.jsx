bc058d8c5cd7fd2da26acce11456ded30ba9576b
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-08-01 06:25:21 +0300

    feat:add about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,7 +6,14 @@ export default function About() {
   return (
     <div className='flex-center h-[97vh] bg-Cgreen-500 '>
       <Container>
-        <div className='flex-center flex-col '><Heading text='about component'/></div>
+        <div className='flex-center flex-col mt-10'>
+          <Heading text='about component' />
+          <div className='row px-10 sm:px-0'>
+            <p className='w-full ps-5 mb-4 md:mb-0 md:w-1/2'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+            <p className='w-full px-5 md:w-1/2'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+            </p>
+          </div>
+        </div>
       </Container>
     </div>
   )
