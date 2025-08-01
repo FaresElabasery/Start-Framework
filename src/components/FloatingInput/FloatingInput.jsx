ab0fef3b1f74714e74ea5f66b258e40c51bafbb7
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-08-01 08:49:54 +0300

    feat: enhance Contact page and add Separated floatingInput component

---
+++ src/components/FloatingInput/FloatingInput.jsx
@@ -0,0 +1,13 @@
+
+export default function FloatingInput({id, type, label, handleFocus, i}) {
+    return (
+        <div className='flex flex-col gap-5 mt-10 text-Cgray-200 ' key={i}>
+            <div className=' w-full relative '>
+                <input aria-label={id} onInput={(e) => handleFocus(e)} className='w-full outline-0 border-b-1 pt-4 pb-2 rounded-lg px-2 border-b-Cgray-200/10' type={type} id={id} placeholder={id} />
+                <div className='py-4 h-full w-full  absolute bottom-full start-0  overflow-hidden'>
+                    <label className='absolute translate-y-10 transition-all duration-400 ease-in-out text-Cgreen-500' htmlFor={id}>{label}</label>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,17 @@
 
 import Container from './../../components/Container/Container';
 import Heading from './../../components/Heading/Heading';
+import FloatingInput from './../../components/FloatingInput/FloatingInput';
 export default function Contact() {
+  // feilds of input for looping 
+  const fields = [
+    { id: "userName", type: "text", label: "User Name" },
+    { id: "userAge", type: "number", label: "User Age" },
+    { id: "userEmail", type: "email", label: "User Email" },
+    { id: "userPassword", type: "password", label: "User Password" }
+  ];
 
+  // handle focus to animate label
   const handleFocus = (e) => {
     const target = e.target;
     target.nextElementSibling.children[0].classList.add('translate-y-0');
@@ -18,38 +27,9 @@ export default function Contact() {
         <div className='flex-center flex-col mt-16'>
           <Heading text='conatct section' color='dark' />
           <div className='w-full md:w-[700px]'>
-            <div className='flex flex-col gap-5 mt-10 text-Cgray-200 '>
-              <div className=' w-full relative '>
-                <input aria-label="userName" onInput={(e) => handleFocus(e)} className='w-full outline-0 border-b-1 pt-4 pb-2 rounded-lg px-2 border-b-Cgray-200/10' type="text" id='userName' placeholder='userName' />
-                <div className='py-4 h-full w-full  absolute bottom-full start-0  overflow-hidden'>
-                  <label className='absolute translate-y-10 transition-all duration-400 text-Cgreen-500' htmlFor="userName">userName :</label>
-                </div>
-              </div>
-            </div>
-            <div className='flex flex-col gap-5 mt-10 text-Cgray-200'>
-              <div className=' w-full relative '>
-                <input aria-label="userAge" onInput={(e) => handleFocus(e)} className='w-full outline-0 border-b-1 pt-4 pb-2 rounded-lg px-2 border-b-Cgray-200/10' type="number" id='userAge' placeholder='userAge' />
-                <div className='py-4 h-full w-full  absolute bottom-full start-0  overflow-hidden'>
-                  <label className='absolute translate-y-10 transition-all duration-400 text-Cgreen-500' htmlFor="userAge">userAge :</label>
-                </div>
-              </div>
-            </div>
-            <div className='flex flex-col gap-5 mt-10 text-Cgray-200'>
-              <div className=' w-full relative '>
-                <input aria-label="userEmail" onInput={(e) => handleFocus(e)} className='w-full outline-0 border-b-1 pt-4 pb-2 rounded-lg px-2 border-b-Cgray-200/10' type="email" id='userEmail' placeholder='userEmail' />
-                <div className='py-4 h-full w-full  absolute bottom-full start-0  overflow-hidden'>
-                  <label className='absolute translate-y-10 transition-all duration-400 text-Cgreen-500' htmlFor="userEmail">userEmail :</label>
-                </div>
-              </div>
-            </div>
-            <div className='flex flex-col gap-5 mt-10 text-Cgray-200'>
-              <div className=' w-full relative '>
-                <input aria-label="userPassword" onInput={(e) => handleFocus(e)} className='w-full outline-0 border-b-1 pt-4 pb-2 rounded-lg px-2 border-b-Cgray-200/10' type="password" id='userPassword' placeholder='userPassword' />
-                <div className='py-4 h-full w-full  absolute bottom-full start-0  overflow-hidden'>
-                  <label className='absolute translate-y-10 transition-all duration-400 text-Cgreen-500' htmlFor="userPassword">userPassword :</label>
-                </div>
-              </div>
-            </div>
+            {fields.map(({ id, type, label }, i) => (
+              <FloatingInput id={id} type={type} label={label} handleFocus={(e)=>handleFocus(e)} i={i} />
+            ))}
             <button className='custom-btn active cursor-pointer mt-10 me-auto'>send Message</button>
           </div>
         </div>
